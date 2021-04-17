b028b3aed4bec1b83237ba344d99a2f03746d6af
kinacororinn <kinacororinn@gmail.com>
2021-04-17 11:39:42 +0000

    課題3途中

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ad14d11dbdf263b7dc21cf7ca8e4a118601dac6a7597380224c36995a7cf5edbbddc4f3367ddc6018ed113819338f466f4273d7ad37ae78b46d5b6d0fc7a3d47'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
+   resources :users, only: [:show, :edit, :update]
+  get 'about' => 'books#about'
+  
 end
